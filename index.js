e917cc64f1702cb1b3fefd79595706cb9c62c2e7
Hasibul Hasan <34993664+Hasibul-99@users.noreply.github.com>
2024-09-16 12:17:31 +0600

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Function to set the body's opacity
 function setBodyOpacity(opacity) {
-    console.log("hello -----");
-    
-    if (opacity >= 0 && opacity <= 1) {
-        document.body.style.opacity = opacity;
-    } else {
-        console.error("Opacity must be between 0 and 1.");
-    }
+    document.body.style.opacity = opacity;
 }
 
 // Example usage: Set opacity to 0.8 (80%)
-setBodyOpacity(0.8);
+setBodyOpacity(0.1);
