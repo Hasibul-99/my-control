b139bdc5b0cc2659231061d49b0097d658c52e7d
Hasibul Hasan <34993664+Hasibul-99@users.noreply.github.com>
2024-09-22 14:13:22 +0600

    Update index.js
    update

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-// Function to set the body's opacity
-function setBodyOpacity(opacity) {
-    document.body.style.opacity = opacity;
-}
 
-// Example usage: Set opacity to 0.8 (80%)
-setBodyOpacity(1);
